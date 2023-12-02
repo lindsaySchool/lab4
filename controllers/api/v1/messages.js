384a264118b16372c1a59f00baf22721174e535a
lindsaySchool <r0883883@student.thomasmore.be>
2023-12-02 18:27:03 +0100

    feat: extend the get function

--- controllers/api/v1/messages.js
+++ controllers/api/v1/messages.js
@@ -3,25 +3,64 @@ const Message = require('../../../models/messages');
 
 //functie
 const getAll = (req, res) => {
+    let user = req.body.user;
+
+    if(user){
+        //GET /api/v1/messages?user=naam
+        Message.find({ "user": user })
+        //iets teruggeven
+        .then(docs => {
+            res.json({ 
+                "status": `GETTING messages for username ${user}`,
+                "data": {
+                    "message": docs.map(doc => doc.text)
+                } 
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }else{
+        //GET /api/v1/messages
+        Message.find()
+        //iets teruggeven
+        .then(docs => {
+            res.json({ 
+                "status": "GETTING messages",
+                "data": {
+                    "message": docs.map(doc => doc.text)
+                } 
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
+    
+}
+//GET /api/v1/messages/:id
+const getById = (req, res) => {
+    let id = req.params.id;
+    Message.findById(id)
     //iets teruggeven
-    Message.find({ "user": "Jan" })
-    .then(docs => {
+    .then(doc => {
         res.json({ 
-            "status": "succes",
+            "status": "GETTING message with id " + id,
             "data": {
-                "message": docs.map(doc => doc.text)
+                "message": doc.text
             } 
         });
     })
     .catch(err => {
         console.log(err);
     });
+
 }
 
 const create =  (req, res, next) => {
     let message = new Message();
-    message.text = req.body.text;
     message.user = req.body.user;
+    message.text = req.body.text; 
     message.added = new Date();
     message.save()
     .then(doc => {
@@ -45,4 +84,5 @@ const create =  (req, res, next) => {
 
 //export the controller
 module.exports.getAll = getAll;
+module.exports.getById = getById;
 module.exports.create = create;
--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -8,8 +8,17 @@ const messagesController = require('../../../controllers/api/v1/messages');
 // GET /api/v1/messages
 router.get("/", messagesController.getAll);
 
+//GET /api/v1/messages/:id
+router.get("/:id", messagesController.getById);
+
 // POST /api/v1/messages
 router.post("/", messagesController.create);
+
+// PUT /api/v1/messages/:id
+//router.put("/:id", messagesController.update);
+
+// DELETE /api/v1/messages/:id
+//router.delete("/:id", messagesController.delete);
 
 //export the router
 module.exports = router;
